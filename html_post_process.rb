b250f9522ca4af059a158d796193105dc29aaf92
Dmytro Nechepurenko <d.nechepurenko@astoundcommerce.com>
2016-04-14 19:23:23 +0300

    Change color names, update post_process_script.

--- html_post_process.rb
+++ html_post_process.rb
@@ -4,12 +4,11 @@ module HtmlPostProcess
       app.after_build do |builder|
         Dir.chdir(build_dir) do
           Dir.glob('**/*.html') do |file|
-            postprecess=File.read(file)
-            remove_bracket=postprecess.gsub('-premailer-cellpadding: 0;', '').gsub('-premailer-cellspacing: 0;', '').gsub(': ', ':').gsub('; ' , ';').gsub(', sans' , ',sans')
+            remove_strings=File.read(file).gsub('-premailer-cellpadding: 0;', '').gsub('-premailer-cellspacing: 0;', '').gsub(': ', ':').gsub('; ' , ';').gsub(', sans' , ',sans')
             File.open(file, "w") do |f|
-              f.write(remove_bracket)
+              f.write(remove_strings)
             end
-            builder.say_status :postprecess, "#{file}"
+            builder.say_status "postprecess", "#{file}"
           end
         end
       end
